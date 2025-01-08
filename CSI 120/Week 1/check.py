727abe02f7d7516dc76ebf8fcba06b465bfc3a4f
zulfa2024 <zulfaoumer1@gmail.com>
2025-01-08 13:28:31 -0800

    Update check.py

---
+++ CSI 120/Week 1/check.py
@@ -0,0 +1,7 @@
+
+#This program will check if a number is greater than 10 or not
+number_1 =10
+if number_1 > 10:
+    print("This number is greater than10") 
+else:
+    print("This number is not greater than 10")
