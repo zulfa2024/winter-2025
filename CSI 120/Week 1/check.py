9bafaad0bfda4f99a2f79faed0df48ded7752f14
zulfa2024 <zulfaoumer1@gmail.com>
2025-01-11 19:09:40 -0800

    Update check.py

--- CSI 120/Week 1/check.py
+++ CSI 120/Week 1/check.py
@@ -7,3 +7,10 @@ elif number_1 < 45:
     print("This number is less than 45")
 elif number_1 == 45:
     print("This number is  same as 45")
+#This program will check if a number is greater than 8 or not
+
+number_2 = 10
+if number_2 > 8:
+    print("This number is greater than 8") 
+elif number_2 < 8:
+    print("This number is less than 8")
