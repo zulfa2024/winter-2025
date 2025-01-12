aa123bfea6418f9d43d2f1d1caa28a2716c8a18f
zulfa2024 <zulfaoumer1@gmail.com>
2025-01-11 20:04:34 -0800

    Update check.py

--- CSI 120/Week 1/check.py
+++ CSI 120/Week 1/check.py
@@ -14,3 +14,12 @@ if number_2 > 8:
     print("This number is greater than 8") 
 elif number_2 < 8:
     print("This number is less than 8")
+#determine the weather condition based on temperature
+
+temperature = 75
+if temperature > 80:
+    print("It's hot outside")
+elif temperature < 60:
+    print("It's getting chilly outside")
+else:
+    print("It's cold outside")
