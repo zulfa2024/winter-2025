5ac2ab0875f2943f6b6ff60e2f3649f70336ea7c
zulfa2024 <zulfaoumer1@gmail.com>
2025-01-13 18:55:45 -0800

    Create ex-1.py

---
+++ CSI 120/Week 1/week2/ex-1.py
@@ -0,0 +1,39 @@
+#assign a number to a variable
+number = 10
+if number > 0:
+    print("number is positive")
+elif number < 0:
+    print("number is negative")
+else:
+    print("number is 0")
+
+#assign a number to a variable
+input = 100
+if input >= 80:
+    print("your grade is B")
+elif input >= 90:
+    print("your grade is A")
+elif input > 70:
+    print("your grade is c")
+else: 
+    print("your grade is D")
+#to display a letter grade
+
+score = 45
+if score >= 90:
+    print("your grade is A")
+elif score >= 80 and score > 80:
+    print("your grade is B")
+elif score >= 70 and score > 70:
+    print("your grade is C")
+elif score >= 60 and score > 60:
+    print("your grade is D")
+else:
+    print("your grade is F")
+
+
+
+
+    
+          
+          
