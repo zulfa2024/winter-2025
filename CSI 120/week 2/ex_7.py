b0e3b601b7323ccb0f3c02e5461d5300c6f807aa
zulfa2024 <zulfaoumer1@gmail.com>
2025-01-17 21:12:48 -0800

    Create ex_7.py

---
+++ CSI 120/week 2/ex_7.py
@@ -0,0 +1,11 @@
+#check if students passed the class. minimum passing score is 73.Also check if entered score is within the range.
+Total=100
+minimumScore=73
+
+score=int(input("Enter your score: "))
+
+if score >=73:
+    print("Congratulations! You passed the class.")
+    
+else:
+    print("Sorry, you failed the class.")   
