fadab553cf4574cba43b1a10a90715e83c477771
zulfa2024 <zulfaoumer1@gmail.com>
2025-01-22 14:05:07 -0800

    Create string.py

---
+++ CSI 120/week3/string.py
@@ -0,0 +1,19 @@
+import os
+os.system('cls')
+a = 10
+b = 10.2
+bb=int(b)
+c="John"
+d = True
+#Type of a is int; a =10
+#Type of b is float; b = 20.1
+#Type of bb is int; bb = 20
+#Type of c is string; c = John
+#Type of d is bool; d = True
+
+print("Type: "+str(type(a))+"a="+str(a))
+      
+
+
+
+
