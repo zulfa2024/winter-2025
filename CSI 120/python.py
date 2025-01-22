b191442784724efab54b86333a624da4cf677f29
zulfa2024 <zulfaoumer1@gmail.com>
2025-01-22 14:05:03 -0800

    Update python.py

--- CSI 120/python.py
+++ CSI 120/python.py
@@ -1,3 +1,12 @@
-("hello")
-("world")
+import random
+num = random.randint(1,10)
+
+print("Guess the number between 1 and 10: ")
+
+guess = int(input("Enter number between 1 to 10"))
+print("System generated number: "+str(guess))
+if guess == num:
+    print("You won")
+else:
+    print("try again")
 
