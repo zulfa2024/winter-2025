2a10e7ce709dd68c679ce2902186704dd8491245
zulfa2024 <zulfaoumer1@gmail.com>
2025-01-22 21:33:51 -0800

    Create boolean_ex.py

---
+++ CSI 120/week3/boolean_ex.py
@@ -0,0 +1,13 @@
+import os
+
+#Assign a boolean value to a variable
+is_raining = False
+print(type(is_raining))
+#complete program by checking
+# 1. if it is raining then display message:"take an umbrella!"
+# 2. otherwise display message "enjoy the sunshine!""
+
+if is_raining:
+    print("Take an umbrella!")
+else:
+    print("Enjoy the sunshine!")
