44aa0f7fa29ad85f7823e0ca149d019e5b5ea754
zulfa2024 <zulfaoumer1@gmail.com>
2025-01-29 14:08:06 -0800

    Update random_ex7.py

--- CSI 120/week3/random_ex7.py
+++ CSI 120/week3/random_ex7.py
@@ -1,12 +1,40 @@
-import os
-#Check If a Number is Even and Greater Than 10
-def check_number(number):
-    if number > 10 and number % 2 == 0:
-        return f"{number} is even and greater than 10"
-    else:
-        return f"{number} is not both even and greater than 10"
 
-# Example usage
-number_to_check = 14
-result = check_number(number_to_check)
-print(result)
+# Checking If a Number is Even and Greater Than 10. 
+
+#If both conditions are true then display: The number [number] is even and greater than 10.
+
+# otherwise display: The number [number]] is either odd or not greater than 10.
+
+import os
+
+os.system('cls')
+
+# Prompt the user to enter a number and convert the input to an integer
+
+num = int(input("Enter a number: "))
+
+
+
+# Check if the number is even and greater than 10
+
+if num % 2 == 0 and num > 10:
+
+    print(f"The number {num} is even and greater than 10.")
+elif num % 2!= 0 and num < 10:
+
+    print(f"The number {num} is odd and less than 10.")
+elif num % 2== 0 and num < 10:
+
+    print(f"The number {num} is even and less than 10.")
+elif num % 2!= 2 and num > 10:
+
+    print(f"The number {num} is even and less than 10.")
+
+elif num % 2!= 0 and num >10:
+    print(f"The number {num} is odd and greater than 10.")
+
+else:
+
+    print(f"The number {num} is either odd or not greater than 10.")
+
+1
