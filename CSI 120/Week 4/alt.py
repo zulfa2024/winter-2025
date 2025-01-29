c97dda4df945cb27087052d28c378c1b5966a98e
zulfa2024 <zulfaoumer1@gmail.com>
2025-01-29 14:08:16 -0800

    Create alt.py

---
+++ CSI 120/Week 4/alt.py
@@ -0,0 +1,13 @@
+#check if entered name is the same as stored name in the program. Capitalization will be ignored
+#jaCob = Jacob = JACOB
+import os
+stored_name = "jacob"
+#accept input from the user 
+input_name = input("Enter name:") # Jacob
+stored_name = stored_name.lower() # jaCob
+input_name = input_name.lower() # JACOB
+if input_name == stored_name: 
+    print("Name matches")
+else:
+    print("Name does not match")
+
