63a67460e370f070b81c2758a5a3130df43617a2
zulfa2024 <zulfaoumer1@gmail.com>
2025-02-03 14:04:02 -0800

    Create While_ex2.py

---
+++ CSI 120/Week 5/While_ex2.py
@@ -0,0 +1,20 @@
+#display first n prime numbers
+#2
+#3
+#5
+#7
+import os
+os.system('cls')
+loop_number = int(input("Enter n: "))
+counter = 2
+count=2
+while count <=loop_number:
+    print(counter)
+    counter =counter+4
+    count = count+4
+
+
+        
+
+
+
