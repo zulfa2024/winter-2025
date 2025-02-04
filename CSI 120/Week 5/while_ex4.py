c5da99163a7d3836168d85ae44d17d6b02737df5
zulfa2024 <zulfaoumer1@gmail.com>
2025-02-03 21:37:31 -0800

    Update while_ex4.py

--- CSI 120/Week 5/while_ex4.py
+++ CSI 120/Week 5/while_ex4.py
@@ -1,5 +1,6 @@
-#Display first n prime numbers
+# Display first n prime numbers
 import os
+os.system('cls')
 n = int(input('Enter the number of prime numbers to display: '))
 count = 0
 num = 2
@@ -17,5 +18,20 @@ while count < n:
         count += 1
     num += 1
 
+# Check if a specific number is prime
+num_to_check = int(input('Enter a number to check if it is prime: '))
+i = 2
+is_prime = True
+
+while i * i <= num_to_check:
+    if num_to_check % i == 0:
+        is_prime = False
+        break
+    i += 1
+
+if is_prime and num_to_check > 1:
+    print(f"{num_to_check} is a prime number")
+else:
+    print(f"{num_to_check} is not a prime number")
 
     
