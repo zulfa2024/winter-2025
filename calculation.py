2de6e14e0823acbf36279912c5d4f7322869f3bd
zulfa2024 <zulfaoumer1@gmail.com>
2025-02-08 21:00:59 -0800

    Create calculation.py

---
+++ calculation.py
@@ -0,0 +1,11 @@
+# Calculation
+result = 3 + 4 * 5
+
+# Output the result
+print("The result is:", result)
+
+# use users input instead
+result = (3+4)*5 
+
+# Output the result
+print("The result is:", result)
