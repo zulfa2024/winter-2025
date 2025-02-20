4e14128a68db36d081a0274c68d17b4d4f160aeb
zulfa2024 <zulfaoumer1@gmail.com>
2025-02-19 19:00:47 -0800

    Create loop2.py

---
+++ CSI 120/week 7/loop2.py
@@ -0,0 +1,14 @@
+import os
+#Sum of first 5 natural numbers
+
+n = 5  # Change this value as needed
+sum_n = 0
+i = 1
+
+while i <= n:
+    sum_n += i
+    i += 1
+
+print("Sum of first", n, "natural numbers is:", sum_n)    #Sum of first 5 natural numbers is: 15
+#print("Sum of first "+str(n)+"natural numbers is: "+str(sum_n))    
+#print(f"Sum of first {n} natural numbers is: {sum_n}")  
