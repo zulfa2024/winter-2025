4e843991c08019ac6bc9e5fdde9d69b5b9796c42
zulfa2024 <zulfaoumer1@gmail.com>
2025-02-19 19:00:54 -0800

    Create loop3.py

---
+++ CSI 120/week 7/loop3.py
@@ -0,0 +1,11 @@
+import os
+#Print Even Numbers up to N
+
+import os
+os.system('cls')
+n = 10  
+num = 2  # Start from the first even number
+
+while num <= n:
+    print(num, end=" ")
+    num += 2  # Increment by 2 to get the next even number
