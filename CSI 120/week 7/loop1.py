42d3bb9bb95361fa57e458f0984552869df2358e
zulfa2024 <zulfaoumer1@gmail.com>
2025-02-19 19:00:43 -0800

    Create loop1.py

---
+++ CSI 120/week 7/loop1.py
@@ -0,0 +1,11 @@
+import os
+
+#using a while loop, print numbers from 1 to 5:
+    
+# Initialize the counter
+num = 1
+
+# While loop to print numbers from 1 to 5
+while num <= 5:
+    print(num)
+    num += 1  # Increment the counter
