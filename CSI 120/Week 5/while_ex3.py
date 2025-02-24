9a903c8fce943e033695ba9134b821a94f7a2017
zulfa2024 <zulfaoumer1@gmail.com>
2025-02-23 21:27:33 -0800

    Update while_ex3.py

--- CSI 120/Week 5/while_ex3.py
+++ CSI 120/Week 5/while_ex3.py
@@ -1,38 +1,15 @@
-# display first n prime numbers
-# function to check if a number is prime
-#5
-#7
-import os
-os.system('cls')
-n = int(input('Enter the number of prime numbers to display: '))
-count = 0
-num = 2
+# Initialize the matrix and the starting values
+matrix = []
+start = 1
+end = 3
 
-while count < n:
-    is_prime = True
-    i = 2
-    while i * i <= num:
-        if num % i == 0:
-            is_prime = False
-            break
-        i += 1
-    if is_prime:
-        print(num)
-        count += 1
-    num += 1
-num = int(input('Enter number:'))
-i = 2
-flag  = False
-counter  = int(input("Enter number: "))
-count = 0
-while count <counter:
-    num = 4
-    while i<num:
-       if num %i ==0:
-         flag = True
-         break   
-       i=i+1    
-    if flag == False:
-        print("it is not a prime number")
-    else:
-      print("it is a prime number")
+# Use a while loop to generate the matrix
+while end <= 7:
+    row = list(range(start, end + 1))
+    matrix.append(row)
+    start += 2
+    end += 2
+
+# Print the matrix
+for row in matrix:
+    print(row)
