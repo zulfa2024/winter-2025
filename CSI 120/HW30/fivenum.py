8de27c683c68743c44e3af0bb1024b0d1d1bf442
zulfa2024 <zulfaoumer1@gmail.com>
2025-02-24 14:08:51 -0800

    Create fivenum.py

---
+++ CSI 120/HW30/fivenum.py
@@ -0,0 +1,9 @@
+import os
+
+os.system("cls")   
+    
+#WRITE A PROGRAM to display first 20 natural numbers. skips all the numbers that are divisible by 5.
+for i in range(1,21):
+    if i%5==0:
+        continue
+    print(i, end=" ")  
