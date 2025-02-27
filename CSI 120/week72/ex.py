57a6fc62eebd084c993ee4d0c1f452f4f375e494
zulfa2024 <zulfaoumer1@gmail.com>
2025-02-26 23:35:16 -0800

    Update ex.py

--- CSI 120/week72/ex.py
+++ CSI 120/week72/ex.py
@@ -1,15 +1,28 @@
 
 import os
+total_items = 3
 # Initialize the list
 cars = ['Audi', 'Fiat', 'Audi']
 
 # Print the initial list
 print("Initial list of cars:", cars)
+# Remove the last item using pop
 
-# Remove all instances of 'Audi' from the list
-print("*****Remove all instances of 'Audi' from cars list***********")
-while "Audi" in cars:
-    cars.remove("Audi")
+print("*****Remove last items from cars_copy List***********")
 
-# Print the final list
+deleted_item = cars.pop()
+
 print(cars)
+
+ # Remove an item by value
+
+print("*****Remove Audi from cars list***********")
+
+cars.remove("Audi")
+
+print(cars) 
+
+ # Print the final list
+print(cars)   
+
+
