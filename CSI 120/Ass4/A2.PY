19ac7613f240dc04bd9600f53463e201d65114a1
zulfa2024 <zulfaoumer1@gmail.com>
2025-02-26 21:40:34 -0800

    Create A2.PY

---
+++ CSI 120/Ass4/A2.PY
@@ -0,0 +1,16 @@
+import os
+os.system("cls")
+
+start_num = 1
+c = 49
+rows = 5
+i = 0
+while i < rows:
+    j = 0
+    while j < 5:
+        print(start_num + j, end=" ")
+        j += 1
+    print("\n")
+    start_num += c
+    c*= 10
+    i += 1
