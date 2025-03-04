3baa690e92393f440446def680323af5447b98fa
zulfa2024 <zulfaoumer1@gmail.com>
2025-03-03 22:44:11 -0800

    Update rem_dupilicate_items_2.py

---
+++ CSI 120/week-9/rem_dupilicate_items_2.py
@@ -0,0 +1,8 @@
+# remove all occurrences of the specified item
+import os
+os. system ("cls")
+counter = 0
+list_length = int(input("How many times are in your list"))
+
+user_list = [] # initialize an empty list to store the items
+
