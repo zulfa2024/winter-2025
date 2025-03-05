3f55799853b30a123bbf618717f154773c573195
zulfa2024 <zulfaoumer1@gmail.com>
2025-03-05 13:55:55 -0800

    Create inClass-ex.py

---
+++ CSI 120/week-9/inClass-ex.py
@@ -0,0 +1,34 @@
+import os
+os.system("cls")
+#create a tuple 
+my_tuple = (1,2,3,4,5,)
+print(my_tuple)
+#create empty tuple
+empty_tuple = ()
+
+#Tuple with one element
+single_tuple = ("10",)
+print(type(single_tuple))
+#without comma
+single_tuple = ("10")
+print(type(single_tuple))
+#tuple with different Data types
+mixed_tuple = (10,"Hello", 3.14, True)
+#Tuple without parentheses
+tuple_packed = 1,2,3,"pythons"
+print(tuple_packed)
+#accessing tuples
+numbers = (10,20.230,40)
+print(numbers[-1])
+
+#negative indexing
+print(numbers[-1])
+#slicing tuple
+print(numbers[1:3])
+a = numbers[1:3]
+a = numbers[1:3]
+print(a)
+print(type(a))
+#tuple immutability
+
+
