089998a3c5abd06fe3c1597c2bda55b86d834499
zulfa2024 <zulfaoumer1@gmail.com>
2025-03-10 13:32:58 -0800

    Create 4.py

---
+++ CSI 120/samary/4.py
@@ -0,0 +1,22 @@
+#print multiline collections
+import os
+
+os.system("cls")
+'''
+(1,1)
+(1,2)
+(1,3)
+(2,1)
+(2,2)
+(2,3)
+(3,1)
+(3,2)
+(3,3)
+
+'''
+
+for i in range(1,4):
+    for j in range(1,4):
+        print(f'({i},{j})')
+    
+        
