14bbd472ea19b3d2d407de3f33a9f9f392ae291f
zulfa2024 <zulfaoumer1@gmail.com>
2025-03-21 16:08:46 -0800

    Create 3.py

---
+++ CSI 120/final3/3.py
@@ -0,0 +1,11 @@
+
+# Program to print the multiplication table from 1 to 10
+# Outer loop for rows
+for i in range(1, 11):
+    # Inner loop for columns
+    for j in range(1, 11):
+        print(f"{i * j:4}", end="")
+    print()  # Moves to the next line after each row
+    
+()
+    
