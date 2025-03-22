d4802bdb461e73d266043fabd3a0ff1b2453fcbb
zulfa2024 <zulfaoumer1@gmail.com>
2025-03-22 01:08:59 -0800

    Update 5.py

--- CSI 120/pyfolder/5.py
+++ CSI 120/pyfolder/5.py
@@ -1,22 +1,16 @@
+# Randomly generate a number between 1 and 20 and prompt the user to guess the number.
 import os
 os.system("cls")  
 import random
-
-# Randomly generate a number between 1 and 20
 target_number = random.randint(1, 20)
-
-# Loop until the user guesses correctly
 correct_guess = False
 while not correct_guess:
-    # Ask the user for a guess
     guess = input("Guess a number between 1 and 20: ")
-    
-    # Ensure the input is a number
     if guess.isdigit():
         guess = int(guess)
         if guess == target_number:
             print("Congratulations! You guessed the correct number.")
-            correct_guess = True  # Exit the loop
+            correct_guess = True  
         elif guess < target_number:
             print("Too low! Try again.")
         else:
